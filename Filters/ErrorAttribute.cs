1db557a8c0aae107ab5173033ac265f360e901b9
Mahmoud Reda <mahmoudreda20030@gmail.com>
2025-01-16 14:00:59 +0200

    Init

---
+++ Filters/ErrorAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ElSayedHotel.Filters
+{
+    public class ErrorAttribute : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            context.Result = new ContentResult() { Content = "Error Occures"};
+        }
+    }
+}
